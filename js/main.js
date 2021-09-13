67bf8db65710cdbfede6fb2b81a842684a793069
Geo-hot <55334979+Shrapnel-max@users.noreply.github.com>
2021-09-13 05:24:04 +0200

    Scroll Active section bugs fixing

--- js/main.js
+++ js/main.js
@@ -28,11 +28,11 @@ const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", scrollActive);
 
 function scrollActive() {
-  const scriollY = window.pageYOffset;
+  const scrollY = window.pageYOffset;
 
   sections.forEach(current => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.oofsetTop - 50;
+    const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
